b15c6fd51107bed0fe9a2be246d359a1b8ee259e
rmsmartins <52045144+rmsmartins@users.noreply.github.com>
2022-06-21 02:20:37 +0100

    Configurações iniciais, utilização do ejs view engine e criação dos formulários

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+
+app.set('view-engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', {name: 'Rui'})
+})
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+
+app.post('/login', (req, res) => {
+    
+})
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs')
+})
+
+app.post('/register', (req, res) => {
+
+})
+
+app.listen(3000)
