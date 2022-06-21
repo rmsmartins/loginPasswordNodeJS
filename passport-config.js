de0f6366ce39bf13883a8b8919a6daba8b209240
rmsmartins <52045144+rmsmartins@users.noreply.github.com>
2022-06-21 23:37:51 +0100

    app.use flash e app.use session

--- passport-config.js
+++ passport-config.js
@@ -3,11 +3,21 @@ const { authenticate } = require('passport')
 const localStrategy = require('passport-local').Strategy
 const bcrypt = require('bcrypt')
 
-function initialize(passport) {
+function initialize(passport, getUserByEmail) {
     const authenticateUser = (email, passord, done) => { 
         const user = getUserByEmail(email)
         if(user  == null) {
             return done(null, false, {message: 'Não existe utilizador com esse email'})
+        }
+
+        try{
+            if (await bcrypt.compare(password, user.passord)) {
+                return done(null, user)
+            } else {
+                return done(null, false, { message: 'Password incorreta' })
+            }
+        } catch {
+            return done(e)
         }
     }
 
@@ -16,3 +26,5 @@ function initialize(passport) {
     passport.serializaUser((user, done) => {})
     passport.deserializaUser((id, done) => {})
 }
+
+modules.exports = initialize 
--- server.js
+++ server.js
@@ -2,14 +2,23 @@ const express = require('express')
 const app = express()
 const bcrypt = require('bcrypt')
 const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
 
 const initializePassport = require('./passport-config')
-initializePassport(passport)
+initializePassport(
+    passport,
+    email => users.find(user => user.email === email)
+    )
 
 const users =[]
 
 app.set('view-engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session({
+    secret: PerformanceObserverEntryList.env.SESSION_SECRET
+}))
 
 app.get('/', (req, res) => {
     res.render('index.ejs', {name: 'Rui'})
