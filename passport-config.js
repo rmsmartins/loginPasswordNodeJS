61397c4422edb26d4247edc5894c83ab7bd0910d
rmsmartins <52045144+rmsmartins@users.noreply.github.com>
2022-06-22 01:47:33 +0100

    Correção de erros

--- passport-config.js
+++ passport-config.js
@@ -3,7 +3,7 @@ const { authenticate } = require('passport')
 const localStrategy = require('passport-local').Strategy
 const bcrypt = require('bcrypt')
 
-function initialize(passport, getUserByEmail) {
+function initialize(passport, getUserByEmail, getUserById) {
     const authenticateUser = async (email, password, done) => { 
         const user = getUserByEmail(email)
         if(user  == null) {
@@ -11,7 +11,7 @@ function initialize(passport, getUserByEmail) {
         }
 
         try{
-            if (bcrypt.compare(password, user.passord)) {
+            if (bcrypt.compare(password, user.password)) {
                 return done(null, user)
             } else {
                 return done(null, false, { message: 'Password incorreta' })
@@ -23,8 +23,10 @@ function initialize(passport, getUserByEmail) {
 
     passport.use(new localStrategy({ usernameField: 'email' },
     authenticateUser))
-    passport.serializeUser((user, done) => { })
-    passport.deserializeUser((id, done) => { })
+    passport.serializeUser((user, done) => done(null, user.id))
+    passport.deserializeUser((id, done) => {
+        return done(null, getUserById(id))}
+        )
 }
 
 module.exports = initialize 
